d8a44d70e4703762539c2bb429dce9a59b5cc1ac
Jatin Singh <jatinsingh098loq2@outlook.com>
2025-09-15 22:47:17 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
 import AnalyzeResume from "./pages/AnalyzeResume";
 import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import ResumeById from "./pages/ResumeById";
 import UploadResume from "./pages/UploadResume";
-import { useAuth } from "./context/AuthContext";
 
 function ProtectedRoute({ children }) {
   const { isLoggedIn, loading } = useAuth();
@@ -23,17 +22,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Public */}
-        <Route path="/login" element={<Login />} />
-
         {/* Protected */}
+        <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
         <Route path="/analyze" element={<ProtectedRoute><AnalyzeResume /></ProtectedRoute>} />
         <Route path="/resume/:id" element={<ProtectedRoute><ResumeById /></ProtectedRoute>} />
         <Route path="/upload" element={<ProtectedRoute><UploadResume /></ProtectedRoute>} />
         <Route path="*" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-
       </Routes>
     </Router>
   );
